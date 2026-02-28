c2dde4120b3b58e1d05f83ce8baaac69d4d4a7fd
Jack Stone <43285700+ScrappieG@users.noreply.github.com>
2026-02-28 13:11:18 -0500

    Cloud Flare integration for frontend/backend (#19)
    * extension added
    
    * cloudflare setup
    
    * add wranglerignore
    
    * adjust gitignore
    
    * remove openAI from modules
    
    * added src dir

---
+++ extension/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="popup">
+      <h1>ArticuLeet</h1>
+      <p className="status">Extension loaded</p>
+    </div>
+  )
+}
+
+export default App
